e24c77e93c548e6ca7a604da6587127667097800
Sebastian Battle <sebastian.battle@proton.me>
2022-12-20 00:09:07 -0500

    feat: refactor day 2 part 1 solve, solve part 2

--- day2.ts
+++ day2.ts
@@ -2,52 +2,43 @@ import { readFileSync } from 'fs'
 
 const INPUT = './inputs/day2.txt'
 
-enum Shape {
-  Rock = 1,
-  Paper = 2,
-  Scissors = 3,
+const scoreMap = {
+  'A X': 4,
+  'A Y': 8,
+  'A Z': 3,
+  'B X': 1,
+  'B Y': 5,
+  'B Z': 9,
+  'C X': 7,
+  'C Y': 2,
+  'C Z': 6,
 }
 
-enum Score {
-  Loss = 0,
-  Draw = 3,
-  Win = 6,
-}
+type key = keyof typeof scoreMap
 
-const ABC = {
-  A: Shape.Rock,
-  B: Shape.Paper,
-  C: Shape.Scissors,
-}
-
-const XYZ = {
-  X: Shape.Rock,
-  Y: Shape.Paper,
-  Z: Shape.Scissors,
-}
-
-function getScore(a: Shape, b: Shape): Score {
-  if (a == b) return Score.Draw
-  if (
-    (a == Shape.Rock && b == Shape.Paper) ||
-    (a == Shape.Paper && b == Shape.Scissors) ||
-    (a == Shape.Scissors && b == Shape.Rock)
-  )
-    return Score.Win
-  else return Score.Loss
+const strategyMap = {
+  'A X': 'A Z',
+  'A Y': 'A X',
+  'A Z': 'A Y',
+  'B X': 'B X',
+  'B Y': 'B Y',
+  'B Z': 'B Z',
+  'C X': 'C Y',
+  'C Y': 'C Z',
+  'C Z': 'C X',
 }
 
 function main(): void {
   const lines = readFileSync(INPUT).toString().split('\n')
-  const values: number[] = []
+  const part1: number[] = []
+  const part2: number[] = []
   lines.forEach((line) => {
-    const [a, b] = line.split(' ') as [keyof typeof ABC, keyof typeof XYZ]
-    const shapeA = ABC[a]
-    const shapeB = XYZ[b]
-    const score = getScore(shapeA, shapeB)
-    values.push(shapeB + score)
+    const key = line as key
+    part1.push(scoreMap[key])
+    part2.push(scoreMap[strategyMap[key] as key])
   })
-  console.log(values.reduce((a, b) => a + b, 0))
+  console.log(part1.reduce((a, b) => a + b, 0))
+  console.log(part2.reduce((a, b) => a + b, 0))
 }
 
 main()
